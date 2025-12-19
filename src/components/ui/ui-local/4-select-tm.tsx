6cd0096e1e3ac6b28636ab2a331a74ab767cd1a6
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-18 19:50:56 -0800

    WIP. component layout tweaks. 3.  add select-tm

---
+++ src/components/ui/ui-local/4-select-tm.tsx
@@ -0,0 +1,54 @@
+import { type ReactNode } from "react";
+import { classNames } from "@/utils";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../shadcn/select";
+
+export type SelectTmTextValue = string | readonly [label: ReactNode, value: string];
+
+type StringValueChangeProps = {
+    value: string;
+    onValueChange: (value: string) => void;
+};
+
+type InputSelectUiProps = Prettify<
+    & {
+        items: readonly SelectTmTextValue[];
+        placeholder?: string;
+        triggerClasses?: string;
+        asPopover?: boolean;
+    }
+    & StringValueChangeProps
+>;
+
+export function SelectTm({ items, value, onValueChange, triggerClasses, placeholder, asPopover }: InputSelectUiProps) {
+    return (
+        <Select value={value} onValueChange={onValueChange}>
+
+            <SelectTrigger className={classNames("px-2 py-1 w-max h-7 text-xs gap-1 cursor-pointer", triggerClasses, "text-mani-foreground bg-mani-background")}>
+                <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+
+            <SelectContent align="start" buttonClasses={popupColorClasses} position={asPopover ? "popper" : "item-aligned"}>
+                {items.map(
+                    (item, idx) => {
+                        const isString = typeof item === 'string';
+                        const label = isString ? item : item[0];
+                        const value = isString ? item : item[1];
+                        return (
+                            <SelectItem className="text-xs" value={value} indicatorFirst key={idx}>
+                                {label}
+                            </SelectItem>
+                        );
+                    })
+                }
+            </SelectContent>
+
+        </Select>
+    );
+}
+
+const popupColorClasses = "\
+h-6 \
+text-primary-900 dark:text-primary-300 \
+bg-primary-100 dark:bg-primary-900";
+
+//const inputAsRefClasses = "text-[0.6rem] text-blue-400! cursor-default";
