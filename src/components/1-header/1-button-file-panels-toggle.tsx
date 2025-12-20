c4581a38f10bd36ee2aa5abd80a1c8e860f0bb33
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-19 18:31:38 -0800

    WIP. update toggle to show file to copy config

--- src/components/1-header/1-button-file-panels-toggle.tsx
+++ src/components/1-header/1-button-file-panels-toggle.tsx
@@ -1,8 +1,9 @@
 import { useSnapshot } from "valtio";
 import { appSettings } from "@/store/1-atoms";
 import { Button } from "@/components/ui/shadcn/button";
-import { SymbolFolder } from "../ui/icons/symbols";
+// import { SymbolFolder } from "../ui/icons/symbols";
 import { cn } from "@/utils";
+import { FolderOpen } from "lucide-react";
 
 export function ButtonToggleFilePanels() {
     const { appUi } = useSnapshot(appSettings);
@@ -11,7 +12,7 @@ export function ButtonToggleFilePanels() {
         <Button
             className={cn(
                 "size-6 focus-visible:ring-0 hover:bg-transparent transition-colors",
-                appUi.showFilePanels ? "text-primary" : "text-muted-foreground/50"
+                appUi.showFilePanels ? "text-primary" : "text-muted-foreground/50",
             )}
             variant="ghost"
             size="icon"
@@ -19,8 +20,8 @@ export function ButtonToggleFilePanels() {
             title={appUi.showFilePanels ? "Hide file panels" : "Show file panels"}
             type="button"
         >
-            <SymbolFolder className="size-4" />
+            {/* <SymbolFolder className="size-4" /> */}
+            <FolderOpen className={cn("size-4", appUi.showFilePanels ? "fill-sky-500/20" : "text-muted-foreground/50")} />
         </Button>
     );
 }
-
